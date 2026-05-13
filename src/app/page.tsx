a75613ca48978ba110c6ce343ad046752f0d01ff
nomade <topleader70@gmail.com>
2026-05-13 16:07:41 +0900

    이름 글자색 청록으로 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
           priority
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-[#22989D]">
             안녕하세요, [솜이아빠]입니다
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
